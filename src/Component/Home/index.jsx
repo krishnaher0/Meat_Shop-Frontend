49ec8d14fba786be8417758229096cde4b4d3bea
krishnaher0 <krishna613460@gmail.com>
2024-08-05 22:22:58 +0545

    home components all added to the screen

---
+++ src/Component/Home/index.jsx
@@ -0,0 +1,16 @@
+import DescBody from "../Pages/DescBody"
+import MainBody from "../Pages/MainBody"
+import LearnMore from "../Pages/LearnMore"
+import Header from "../Pages/Header"
+
+const Home =()=>{
+    return (
+        <>
+        <Header/>
+        <MainBody/>
+        <DescBody/>
+        <LearnMore/>
+        </>
+    )
+}
+export default Home
